b4e2c5401da67f6b195872d4a8ea1b1476d9c52f
GianMarco-20 <75840140gm20@gmail.com>
2025-08-29 00:32:33 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,14 +7,31 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-slate-950 text-white">
+      {/* Navbar */}
       <Navbar />
-      <div className="flex-grow">
-        <AppRoutes />
-      </div>
+
+      {/* Contenido principal */}
+      <main className="flex-grow relative flex">
+        {/* Fondo con imagen */}
+        <div 
+          className="absolute inset-0 bg-cover bg-center" 
+          style={{ backgroundImage: `url('/images/FondoHome.jpg')` }}
+        >
+          <div className="absolute inset-0 bg-gray-900 bg-opacity-70 backdrop-blur-md"></div>
+        </div>
+
+        {/* Contenido centrado EXACTAMENTE */}
+        <div className="relative z-10 flex flex-1 items-center justify-center">
+          <AppRoutes />
+        </div>
+      </main>
+
+      {/* Footer */}
       <Footer />
     </div>
   );
 };
 
 export default App;
+
