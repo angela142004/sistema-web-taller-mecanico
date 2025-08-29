46e6f59bcb350e69248edbe1509db2be7cea7935
GianMarco-20 <75840140gm20@gmail.com>
2025-08-29 01:19:59 -0500

    Update AppRoutes.jsx

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/Home";
 import Register from "../pages/Register";
+import Nosotros from "../pages/Nosotros";
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/register" element={<Register />} />
+    <Route path="/nosotros" element={<Nosotros />} />
   </Routes>
 );
 
